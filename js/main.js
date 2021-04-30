ed99fa50a7048291f1c1105e26619a6609cb5c40
poly28 <horimasa0328@gmail.com>
2021-04-30 11:38:58 +0900

    finish

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 const App1 = {
 	data() {
 		return {
-			message: 'Hello!',
+			itemList: [
+				{ id: 0, text: '0' },
+				{ id: 1, text: '1' },
+				{ id: 2, text: '2' },
+			],
 		};
 	},
 };
+app1 = Vue.createApp(App1);
 
-// app1 = Vue.createApp(App1);
-// app1.mount('#app1');
-Vue.createApp(App1).mount('#app1');
+app1.component('template1', {
+	props: ['prop1'],
+	template: `
+	<li>{{prop1.id}}は{{prop1.text}}番です</li>
+	`,
+});
+
+app1.mount('#app1');
